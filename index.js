4006c66e5f4e0fdf2baa8aed3cada144afcfbf8a
Berto Yáñez <berto@ber.to>
2015-08-07 19:40:27 +0200

    Almost done

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ var currentIndex = __dirname + '/current.json';
 
 var places = require(dataFile);
 
-var next = fs.existsSync(currentIndex) ? require(currentIndex) : {index: 0};
+var next = fs.existsSync(currentIndex) ? require(currentIndex) : {
+    index: 0
+};
 
 var greetings = {
     multi: [
-        'Un saúdo para {{place}}, na parroquia de {{parish}}, concello de {{council}}!'
+        'Un saúdo para {{place}}, na parroquia {{parish}}, concello {{council}}!'
     ],
     councilparish: [
-        'Un saúdo para {{place}}, concello de {{council}}'
+        'Un saúdo para {{place}}, concello {{council}}'
 
     ],
     single: [
@@ -28,23 +30,58 @@ var greetings = {
     ]
 };
 
+var prefixer = function(string) {
+    'use strict';
+
+    var s = S(string);
+
+    var prefixes = [{
+        pre: 'A ',
+        rep: 'da '
+    }, {
+        pre: 'O ',
+        rep: 'do '
+    }, {
+        pre: 'As ',
+        rep: 'das '
+    }, {
+        pre: 'Os ',
+        rep: 'dos '
+    }];
+
+    for (var i = 0; i < prefixes.length; i++) {
+        var pref = prefixes[i];
+
+        if (s.startsWith(pref.pre)) {
+            return pref.rep + s.chompLeft(pref.pre);
+        }
+    }
+
+    return 'de ' + s.s;
+};
+
 var tweet = function() {
     'use strict';
 
     var place = places[next.index];
 
-    var mode = (place.council === place.parish && place.parish === place.place) ? 'single' : (place.council === place.parish) ? 'councilparish' : 'multi';
+    var mode = (place.council === place.parish && place.parish === place.place) ? 'single' : (place.council === place.parish || place.parish === place.place) ? 'councilparish' : 'multi';
 
     var greeting = _.sample(greetings[mode]);
 
+    place.council = prefixer(place.council);
+    place.parish = prefixer(place.parish);
+
     var tweet = S(greeting).template(place).s;
 
-    // Twitter.post('statuses/update', {
-    //     status: tweet
-    // }, function(error, data, response) {
-    //     if (error) {
-    //         throw new Error(error);
-    //     }
+    Twitter.post('statuses/update', {
+        status: tweet
+    }, function(error, data, response) {
+        if (error) {
+            throw new Error(error);
+        }
+
+        console.log(next.index + ' | ' + tweet);
 
         next.index++;
 
@@ -54,15 +91,18 @@ var tweet = function() {
                 process.exit(1);
             }
 
-            console.log(tweet);
         });
-    // });
+    });
 
 };
 
 setInterval(function() {
     'use strict';
-    tweet();
+    try {
+        tweet();
+    } catch (e) {
+        console.log(e);
+    }
 }, 1000 * 60 * 15);
 
 tweet();
