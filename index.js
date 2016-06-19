918ff43082b27aa01c981d25c497fd092ab074ac
Berto Yáñez <berto@ber.to>
2016-06-19 23:18:06 +0200

    Removed interval

--- index.js
+++ index.js
@@ -101,13 +101,13 @@ var tweet = function() {
 
 };
 
-setInterval(function() {
-    'use strict';
-    try {
-        tweet();
-    } catch (e) {
-        console.log(e);
-    }
-}, 1000 * 60 * 15);
+//setInterval(function() {
+//    'use strict';
+//    try {
+//        tweet();
+//    } catch (e) {
+//        console.log(e);
+//    }
+//}, 1000 * 60 * 15);
 
 tweet();
